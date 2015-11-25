b8dc5794eaad7cbf66359207f13df53b0a3a1682
Andrian Budantsov <andrian@readdle.com>
2015-11-25 10:32:25 +0200

    Initial Commit

---
+++ mobilechallenge15-server/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  mobilechallenge15-server
+//
+//  Created by Andrian Budantsov on 11/25/15.
+//  Copyright © 2015 Andrian Budantsov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
